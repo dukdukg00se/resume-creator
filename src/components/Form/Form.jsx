1e8887d5e799fbb4eeee272d3acf2b8b8df45997
Will H <will.wkhu@gmail.com>
2023-05-01 22:34:06 -0700

    Add form components to collect user info

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import Personal from './components/Personal';
+import Experience from './components/Experience';
+import Education from './components/Education';
 
 class Form extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
 
   render() {
     return (
       <form>
-        <Personal />
+        <section>
+          <h2>Personal Information</h2>
+          <Personal />
+        </section>
+        <section>
+          <h2>Experience</h2>
+          <Experience />
+        </section>
+        <section>
+          <h2>Education</h2>
+          <Education />
+        </section>
       </form>
     );
   }
--- src/components/Form/components/Personal.jsx
+++ src/components/Form/components/Personal.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 
 class Personal extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
 
   render() {
     return (
       <div>
-        <h2>Personal Information</h2>
-        <input type="text" placeholder="Name" value=""></input>
-        <input type="" placeholder="Title" value=""></input>
+        <input type="text" placeholder="Name" value={this.props.name} />
+        <input type="text" placeholder="Title" value={this.props.title} />
+        <input type="text" placeholder="Address" value={this.props.address} />
+        <input type="text" placeholder="Phone" value={this.props.phone} />
+        <input type="email" placeholder="Email" value={this.props.email} />
+        <textarea
+          placeholder="Professional Summary"
+          value={this.props.summary}
+        />
       </div>
     );
   }
