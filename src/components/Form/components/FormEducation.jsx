92b79088210e5a32d8f6a4687a96a9fcc6f23236
Will H <will.wkhu@gmail.com>
2023-05-04 14:54:12 -0700

    Add attributes for inputs

--- src/components/Form/components/FormEducation.jsx
+++ src/components/Form/components/FormEducation.jsx
@@ -3,21 +3,41 @@ import React from 'react';
 class FormEducation extends React.Component {
   render() {
     return (
-      <div>
+      <div id="education">
         <input
+          name="degree"
           type="text"
           placeholder="Degree"
-          value={this.props.degree}
+          onChange={this.props.onChange}
+          // value={this.props.degree}
         ></input>
-        <input type="text" placeholder="Major" value={this.props.major}></input>
         <input
+          name="major"
+          type="text"
+          placeholder="Major"
+          onChange={this.props.onChange}
+          // value={this.props.major}
+        ></input>
+        <input
+          name="university"
           type="text"
           placeholder="University"
-          value={this.props.university}
+          onChange={this.props.onChange}
+          // value={this.props.university}
         ></input>
-        <input type="text" placeholder="From" value={this.props.from}></input>
-        <input type="text" placeholder="To" value={this.props.to}></input>
-        <textarea placeholder="Description"></textarea>
+
+        <input
+          name="graduated"
+          type="text"
+          placeholder="Graduated"
+          onChange={this.props.onChange}
+          // value={this.props.graduated}
+        ></input>
+        <textarea
+          name="description"
+          placeholder="Description"
+          onChange={this.props.onChange}
+        ></textarea>
       </div>
     );
   }
