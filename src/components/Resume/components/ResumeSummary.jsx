803ee463feea0cf2f8edca8b5c7e2572fffc9e0c
Will H <will.wkhu@gmail.com>
2023-05-03 23:47:00 -0700

    Add resume subcomponents

---
+++ src/components/Resume/components/ResumeSummary.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class ResumeSummary extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Summary</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit autem
+          temporibus unde cum, excepturi deserunt a quidem voluptate facilis
+          repellat numquam sapiente veritatis magnam aspernatur! Iusto omnis quo
+          atque quam.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default ResumeSummary;
