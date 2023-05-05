9da1d85a23ae4b6651001d043935394c0704aad3
Will H <will.wkhu@gmail.com>
2023-05-05 15:12:35 -0700

    Add page buttons

--- src/components/Form/components/Buttons.jsx
+++ src/components/Form/components/Buttons.jsx
@@ -2,19 +2,31 @@ import React from 'react';
 
 class Add extends React.Component {
   render() {
-    return <button type="button">Add</button>;
+    return (
+      <button type="button" name="add" onClick={this.props.onClick}>
+        Add
+      </button>
+    );
   }
 }
 
 class Clear extends React.Component {
   render() {
-    return <button type="button">Clear</button>;
+    return (
+      <button type="button" name="clear" onClick={this.props.onClick}>
+        Clear
+      </button>
+    );
   }
 }
 
 class Delete extends React.Component {
   render() {
-    return <button type="button">Delete</button>;
+    return (
+      <button type="button" name="delete" onClick={this.props.onClick}>
+        Delete
+      </button>
+    );
   }
 }
 
