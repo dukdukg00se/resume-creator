7757d9e1685ee3412312e245006a35e5596aa2e6
Will H <will.wkhu@gmail.com>
2023-05-06 13:18:24 -0700

    Add value attribute to control form inputs

--- src/components/Form/components/FormExperience.jsx
+++ src/components/Form/components/FormExperience.jsx
@@ -2,46 +2,49 @@ import React from 'react';
 
 class FormExperience extends React.Component {
   render() {
+    // console.log(this.props);
+
     return (
       <div id="experience">
         <input
           name="position"
           type="text"
           placeholder="Position"
-          // value={this.props.position}
+          value={this.props.experience.position}
           onChange={this.props.onChange}
         ></input>
         <input
           name="company"
           type="text"
           placeholder="Company"
-          // value={this.props.company}
+          value={this.props.experience.company}
           onChange={this.props.onChange}
         ></input>
         <input
           name="location"
           type="text"
           placeholder="City"
-          // value={this.props.city}
+          value={this.props.experience.city}
           onChange={this.props.onChange}
         ></input>
         <input
           name="from"
           type="text"
           placeholder="From"
-          // value={this.props.from}
+          value={this.props.experience.from}
           onChange={this.props.onChange}
         ></input>
         <input
           name="to"
           type="text"
           placeholder="To"
-          // value={this.props.to}
+          value={this.props.experience.to}
           onChange={this.props.onChange}
         ></input>
         <textarea
           name="responsibilities"
           placeholder="Responsibilities"
+          value={this.props.experience.responsibilities}
           onChange={this.props.onChange}
         ></textarea>
       </div>
