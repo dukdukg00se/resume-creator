0099e89e8a8a9d1b98da313705244d740b7b85ce
Will H <will.wkhu@gmail.com>
2023-05-06 13:19:00 -0700

    Add value attributes for input control

--- src/components/Form/components/FormPersonal.jsx
+++ src/components/Form/components/FormPersonal.jsx
@@ -9,41 +9,41 @@ class FormPersonal extends React.Component {
           name="name"
           type="text"
           placeholder="Name"
-          // value={this.props.personalInfo.name}
+          value={this.props.personalInfo.name}
           onChange={this.props.onChange}
         />
         <input
           name="title"
           type="text"
           placeholder="Title"
-          // value={this.props.title}
+          value={this.props.personalInfo.title}
           onChange={this.props.onChange}
         />
         <input
           name="address"
           type="text"
           placeholder="Address"
-          // value={this.props.address}
+          value={this.props.personalInfo.address}
           onChange={this.props.onChange}
         />
         <input
           name="phone"
           type="text"
           placeholder="Phone"
-          // value={this.props.phone}
+          value={this.props.personalInfo.phone}
           onChange={this.props.onChange}
         />
         <input
           name="email"
           type="email"
           placeholder="Email"
-          // value={this.props.email}
+          value={this.props.personalInfo.email}
           onChange={this.props.onChange}
         />
         <textarea
           name="summary"
           placeholder="Professional Summary"
-          // value={this.props.summary}
+          value={this.props.personalInfo.summary}
           onChange={this.props.onChange}
         />
       </div>
